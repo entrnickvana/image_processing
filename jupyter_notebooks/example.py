501d1ff3242b3c4723ce05097ddf37e43cb10374
entrnickvana <nick.jwl123@gmail.com>
2021-09-14 00:38:44 -0600

    add jupyter-notebook

---
+++ jupyter_notebooks/example.py
@@ -0,0 +1,29 @@
+
+import skimage
+from skimage import io
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def color2grey(img):
+    b = [.3, .6, .1]
+    return np.dot(img[...,:3], b)
+
+img = io.imread('project1_images/project1_images/houndog1.png')
+
+print(img.shape)
+plt.imshow(img)
+plt.title('Dog')
+plt.show()
+
+gray_img = color2grey(img)
+print(gray_img.shape)
+plt.imshow(gray_img, cmap='gray')
+plt.show()
+
+
+
+
+
+
